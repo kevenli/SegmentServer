e533508172e044054beb6e79960df48b8a2a97f4
Keven Li <pbleester@gmail.com>
2015-08-06 13:53:19 +0800

    init

---
+++ src/affective/SegmentAction.java
@@ -0,0 +1,42 @@
+package affective;
+import java.io.File;
+
+import net.sf.json.JSONObject;
+
+import org.apache.struts2.ServletActionContext;
+
+import com.gta.affective.Segment;
+
+public class SegmentAction {
+	private String text;
+	private float result;
+	
+	public String execute() throws Exception {
+        System.out.println(Thread.currentThread().getContextClassLoader().getResource(""));  
+        
+        System.out.println(SegmentAction.class.getClassLoader().getResource(""));  
+  
+        System.out.println(ClassLoader.getSystemResource(""));  
+        System.out.println(SegmentAction.class.getResource(""));  
+        System.out.println(SegmentAction.class.getResource("/"));
+        //Class文件所在路径
+        System.out.println(new File("/").getAbsolutePath());  
+        System.out.println(System.getProperty("user.dir"));  
+        
+		Segment s = new Segment();
+		result = s.analysis(text);
+		JSONObject jsonObject=new JSONObject();
+        jsonObject.accumulate("status", "ok");
+        jsonObject.accumulate("data", result);
+		ServletActionContext.getRequest().setAttribute("data", jsonObject.toString());
+		return "success";
+	}
+
+	public String getText() {
+		return text;
+	}
+
+	public void setText(String text) {
+		this.text = text;
+	}
+}
